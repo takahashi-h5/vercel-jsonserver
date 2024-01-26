2e5cad30ccaa22b0b8bce35973cdf78065b25cd8
takahashi-h5 <mori@confrage.com>
2024-01-26 13:14:53 +0900

    commit

---
+++ server.js
@@ -0,0 +1,15 @@
+
+const jsonServer = require('json-server')
+const server = jsonServer.create()
+const router = jsonServer.router('db.json')
+const middlewares = jsonServer.defaults()
+server.use(middlewares)
+server.use(
+    jsonServer.rewriter({
+     "/api/*": "/$1",
+    })
+   );
+server.use(router)
+server.listen(3000, () => {})
+
+module.exports = server
